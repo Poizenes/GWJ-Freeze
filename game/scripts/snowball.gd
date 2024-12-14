8e8424a1aad61c58c65a70893c53cd9e368f30df
Poizenes <poizenes@swisscows.email>
2024-12-14 23:16:06 +0100

    Add throw sound effect

--- game/scripts/snowball.gd
+++ game/scripts/snowball.gd
@@ -4,6 +4,7 @@ class_name Snowball
 func _ready() -> void:
 	velocity.x = get_parent().INITIAL_VELOCITY.x
 	velocity.y = get_parent().INITIAL_VELOCITY.y
+	$AudioStreamPlayer2D.play()
 
 func _physics_process(delta: float) -> void:
 	velocity.y += get_parent().GRAVITY
