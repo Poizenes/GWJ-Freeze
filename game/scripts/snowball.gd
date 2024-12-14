0d80edb4aae813f26fedef9ff86baf3caa5063f0
Poizenes <poizenes@swisscows.email>
2024-12-14 20:19:10 +0100

    Remove redundant pass

--- game/scripts/snowball.gd
+++ game/scripts/snowball.gd
@@ -4,7 +4,6 @@ class_name Snowball
 func _ready() -> void:
 	velocity.x = get_parent().INITIAL_VELOCITY.x
 	velocity.y = get_parent().INITIAL_VELOCITY.y
-	pass
 
 func _physics_process(delta: float) -> void:
 	velocity.y += get_parent().GRAVITY
