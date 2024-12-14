4eaad37c819a66775f08a8ed4fc8e70310439d1a
DevCev <github@devcev.net>
2024-12-14 16:39:09 +0100

    Added duck animation

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 @export_group("Animation")
 @export var IDLE_ANIMATION_SPEED_SCALE : float =  0.4
 @export var THROW_ANIMATION_SPEED_SCALE : float = 3
+@export var DUCK_ANIMATION_SPEED_SCALE : float = 2
 
 var ducked := false 
 
@@ -14,11 +15,12 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("duck"):
 		ducked = true
-	else:
+		duck()
+	if event.is_action_released("duck"):
 		ducked = false
+		duck(true)
 	if event.is_action_released("throw"):
 		throw()
-
 
 func on_hit() -> void:
 	if not ducked:
@@ -31,10 +33,19 @@ func throw() -> void:
 func idle() -> void:
 		sprite.play("idle")
 		sprite.speed_scale = IDLE_ANIMATION_SPEED_SCALE
+
+func duck(backwards: bool = false) -> void:
+	if backwards:
+		sprite.play_backwards("duck")
+	else:
+		sprite.play("duck")
+	sprite.speed_scale = DUCK_ANIMATION_SPEED_SCALE
 	
 func on_animation_finished() -> void:
 	if sprite.animation == "throw":
 		var snowball = load("res://scenes/snowball.tscn").instantiate()
 		get_parent().add_child(snowball)
 		snowball.position = position
+		idle()
+	elif sprite.animation == "duck" and sprite.frame == 0:
 		idle()
