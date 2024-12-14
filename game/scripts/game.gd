5090d95bd4cfb5cc0f187f56576bc36d16a0af13
Poizenes <poizenes@swisscows.email>
2024-12-14 23:06:28 +0100

    Add escape button handling
    Game quits when ESC is pressed

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -3,3 +3,7 @@ extends Node2D
 @export_group("Snowball Physics")
 @export var GRAVITY := 1.5
 @export var INITIAL_VELOCITY := Vector2(160, -75)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
