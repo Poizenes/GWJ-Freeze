8450090e3d35e437a5e6525f54a41f0f19422930
Poizenes <poizenes@swisscows.email>
2024-12-14 19:45:54 +0100

    Add stand_up animation
    Replace reversed duck animation with stand_up animation

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 @export var IDLE_ANIMATION_SPEED_SCALE : float =  0.4
 @export var THROW_ANIMATION_SPEED_SCALE : float = 3
 @export var DUCK_ANIMATION_SPEED_SCALE : float = 2
+@export var STAND_UP_ANIMATION_SPEED_SCALE : float = 2
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -17,7 +18,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("duck") && sprite.animation == "idle":
 		duck()
 	elif Input.is_action_just_released("duck"):
-		duck(true)
+		stand_up()
 
 func throw() -> void:
 	sprite.play("throw")
@@ -27,12 +28,13 @@ func idle() -> void:
 		sprite.play("idle")
 		sprite.speed_scale = IDLE_ANIMATION_SPEED_SCALE
 
-func duck(backwards: bool = false) -> void:
-	if backwards:
-		sprite.play_backwards("duck")
-	else:
-		sprite.play("duck")
+func duck() -> void:
+	sprite.play("duck")
 	sprite.speed_scale = DUCK_ANIMATION_SPEED_SCALE
+
+func stand_up() -> void:
+	sprite.play("stand_up")
+	sprite.speed_scale = STAND_UP_ANIMATION_SPEED_SCALE
 
 func on_animation_finished() -> void:
 	if sprite.animation == "throw":
@@ -40,7 +42,7 @@ func on_animation_finished() -> void:
 		get_parent().add_child(snowball)
 		snowball.position = position + Vector2(-2, -4)
 		sprite.play("throw_return")
-	elif sprite.animation == "duck" and sprite.frame == 0:
+	elif sprite.animation == "stand_up":
 		idle()
 	elif sprite.animation == "throw_return":
 		idle()
