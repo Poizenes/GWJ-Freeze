eff402814d8f46e0644f3d78f11f76ce72ea2e3b
Poizenes <poizenes@swisscows.email>
2024-12-16 00:00:00 +0100

    Change snowball curve from Bezier to projectile motion

--- game/scripts/snowball.gd
+++ game/scripts/snowball.gd
@@ -2,36 +2,34 @@ extends CharacterBody2D
 class_name Snowball
 
 @export var sound : AudioStreamPlayer2D
+@export var GRAVITY = 100
 
 @onready var start_position: Vector2
-
-func _ready() -> void:
-	sound.play()
-	start_position = global_position
-
-var t = 0
-func fly_arc(p0: Vector2, p2: Vector2, stretch: float, delta: float, speed: float = 1) -> void:
-	t += delta * speed
-	var p1 = Vector2(0.5 * (p0.x + p2.x), 0.5 * (p0.y + p2.y) + stretch)
-	var q0 = p0.lerp(p1, t)
-	var q1 = p1.lerp(p2, t)
-	position = q0.lerp(q1, t)
-
-func _physics_process(delta: float) -> void:
-	var pedestrian_pos = get_nearest_pedestrian_position(Vector2(160, 120))
-	fly_arc(start_position, pedestrian_pos, -80, delta)
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		var collider = collision.get_collider()
-		if collider.has_method("on_snowball_hit"):
-			collider.call("on_snowball_hit")
-		queue_free()
 
 var nearest_pedestrian_position: Vector2 = Vector2.ZERO:
 	set(new_position):
 		nearest_pedestrian_position = new_position
 	get:
 		return nearest_pedestrian_position
+
+func _ready() -> void:
+	sound.play()
+	start_position = global_position
+	var angle = 5.75
+	var pedestrian_pos = get_nearest_pedestrian_position(Vector2(160, 120))
+	var range = pedestrian_pos.x - position.x
+	var initial_velocity = get_initial_velocity(range, angle)
+	velocity.x = initial_velocity * cos(angle)
+	velocity.y = initial_velocity * sin(angle)
+
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider.has_method("on_snowball_hit"):
+			collider.call("on_snowball_hit")
+		queue_free()
 
 func get_nearest_pedestrian_position(fallback: Vector2 = Vector2.ZERO) -> Vector2:
 	if nearest_pedestrian_position:
@@ -44,3 +42,7 @@ func get_nearest_pedestrian_position(fallback: Vector2 = Vector2.ZERO) -> Vector
 	else:
 		nearest_pedestrian_position = fallback
 	return nearest_pedestrian_position
+	
+	
+func get_initial_velocity(range, angle = 0.8) -> float:
+	return sqrt((range * -GRAVITY)/(sin(2 * angle)))
